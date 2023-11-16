9aed858ee01c0a12c96d20bc179e1d9bf144b4aa
ayasyrev <a.yasyrev@gmail.com>
2023-11-16 18:44:08 +0300

    fix enum in python<3.12, typing for python 3.8

--- src/nbtoolbox/commands.py
+++ src/nbtoolbox/commands.py
@@ -4,9 +4,11 @@
 
 # First version - one command per line.
 # cell command may start from `code:` or just cell command.
+from __future__ import annotations
+
+from collections import defaultdict
 from enum import Enum
 from typing import NamedTuple, Optional
-from collections import defaultdict
 
 
 class CommandLevel(Enum):
@@ -15,13 +17,16 @@ class CommandLevel(Enum):
     OUTPUT = "output"
 
 
+COMMAND_LEVEL_VALUES = tuple(command_level.value for command_level in CommandLevel)
+
+
 class CellCommand(Enum):
     HIDE = "hide"
     COLLAPSE = "collapse"
     COLLAPSIBLE = "collapsible"
 
 
-COMMAND_LEVEL = CommandLevel
+CELL_COMMAND_VALUES = tuple(cell_command.value for cell_command in CellCommand)
 
 
 class Command(NamedTuple):
@@ -55,10 +60,10 @@ def parse_command(line: str) -> Optional[Command]:
     if ":" in line:
         level, command = line[2:].split(":")
         level = level.strip()
-        if level in COMMAND_LEVEL:
+        if level in COMMAND_LEVEL_VALUES:
             return Command(level=level, commands=[command.strip()])
     else:
         command = line[2:].strip()
-        if command in CellCommand:
+        if command in CELL_COMMAND_VALUES:
             return Command(level="cell", commands=[command])
     return None
